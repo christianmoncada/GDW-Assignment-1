95ee921070f5f9726dd267ba0d1b8766be77da12
Arianna Thorson <arianna.thorson@ontariotechu.net>
2020-10-21 15:33:49 -0400

    Start and Finish

---
+++ GDW Assignment 1/GDW Assignment 1/GameOver.cpp
@@ -0,0 +1,7 @@
+using namespace std;
+#include <iostream>
+int main() {
+	std::cout << "GAME OVER\n";
+	std::cout << "Retry?\n";
+	std::cout << "Exit\n";
+}
---
+++ GDW Assignment 1/GDW Assignment 1/Start.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+#include <iostream>
+int main() {
+	std::cout << "Dirty Derby\n";
+	std::cout << "Number of Players :x:\n";
+	std::cout << "Start\n";
+	std::cout << "Exit\n";
+}
