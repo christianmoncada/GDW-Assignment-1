012d0c1c9ed7912ed21ebafe302ef48ca006f15a
Angelina <angelinar0119@gmail.com>
2020-10-23 16:22:15 -0400

    little change

--- GDW Assignment 1/GDW Assignment 1/main.cpp
+++ GDW Assignment 1/GDW Assignment 1/main.cpp
@@ -8,6 +8,9 @@
 
 int main()
 {
+	//start scene here
+
+
 	Board theBoard;
 	Player player2;
 	Player player3;
@@ -19,6 +22,9 @@ int main()
 	player4.InitPlayer(4, 0, theBoard);
 	theBoard.UpdateBoard(player1.GetPosition(), player2.GetPosition(), player3.GetPosition(), player4.GetPosition());
 	bool gamecontinue = true;
+
+	Player winner;
+	bool hasWon = false;
 
 	//determine player order here
 	Player players[4] = { player1, player2, player3, player4 };
@@ -40,22 +46,20 @@ int main()
 			theBoard.UpdateBoard(player1.GetPosition(), player2.GetPosition(), player3.GetPosition(), player4.GetPosition());
 
 			//add check if the player's position is greater than or equal to 100, if it is, then remove that player from the array
+			//and if they are the first person to 100, set them as the winner. Ex make winner = the player that one, and set hasWon = true so we know there is already a winner
 
 		}
 		//end of one turn
 		counter += 1;
 		std::cout << "End of turn " << counter << std::endl;
-
-
+		//means there is only one player left
+		if (std::size(players) <= 1)
+		{
+			std::cout << "Game has ended.\n";
+			gamecontinue = false;
+		}
 
 
 	}
-
-
-
-
-	
-	
-	
-
+	//create ending screen here
 }
