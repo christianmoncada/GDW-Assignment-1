f766d988fe248bbeb56c118f98a2ef3aa62977ed
Arianna Thorson <arianna.thorson@ontariotechu.net>
2020-10-23 22:44:07 -0400

    Sabotage prototype
    Early Sabotage code

--- GDW Assignment 1/GDW Assignment 1/Sabotage.cpp
+++ GDW Assignment 1/GDW Assignment 1/Sabotage.cpp
@@ -1,1 +1,10 @@
 #include "Sabotage.h"
+#include "Player.h"
+#include "Board.h"
+#include <iostream>
+int sabotage
+for (int i(2); i < playercount; ++i) {
+    if (player[0] == player[i]) {
+        std::cout << "Player " << (int number) << " and Player " << (int number) << " are on the same position.";
+    }
+}
--- GDW Assignment 1/GDW Assignment 1/Sabotage.h
+++ GDW Assignment 1/GDW Assignment 1/Sabotage.h
@@ -1,1 +1,11 @@
 #pragma once
+{
+public:
+	int sabotage;
+	int number;
+	Player();
+	Player(int number, int position);
+	int diceroll;
+	int moveBack(position -2);
+	int moveForward(position + 5);
+}
